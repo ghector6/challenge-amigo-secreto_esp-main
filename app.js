226d06e7e6cb6ad7e21f49805eb3c1abc74d16dc
Hector Garcia Rosales <ghectormus@gmail.com>
2025-01-17 16:14:14 -0600

    ops(app.js/agregarAmigo) ADD: fn validating empty string

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ let friendsList = [];
 
 function agregarAmigo() {
   let nombreAmigo = document.getElementById("amigo").value
+  if(nombreAmigo === ""){
+    alert("Por favor, inserte un nombre.")
+  }
 }
