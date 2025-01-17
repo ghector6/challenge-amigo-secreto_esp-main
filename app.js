c7a77dcb30b13d2355482c5c9e2e3aa91194b7e3
Hector Garcia Rosales <ghectormus@gmail.com>
2025-01-17 16:15:52 -0600

    refac(app.js/agregarAmigo) fn validating empty spaces and empty strings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let friendsList = [];
 
 function agregarAmigo() {
   let nombreAmigo = document.getElementById("amigo").value
-  if(nombreAmigo === ""){
+  if(nombreAmigo.replace(/\s/g,"") === ""){
     alert("Por favor, inserte un nombre.")
   }
 }
