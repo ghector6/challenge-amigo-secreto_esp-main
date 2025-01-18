dd0f02935442e6b63e5ce5f472f4ac9b9b79bbff
Hector Garcia Rosales <ghectormus@gmail.com>
2025-01-18 00:32:12 -0600

    ops(app.js/mostrarListaAmigos) fn crear lista y mostrar

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 let friendsList = [];
+let elementoHTML = document.getElementById("listaAmigos")
 
 function agregarAmigo() {
   let nombreAmigo = document.getElementById("amigo").value
   if(nombreAmigo.replace(/\s/g,"") === ""){
     alert("Por favor, inserte un nombre.")
+  }else{
+    friendsList.push(nombreAmigo)
+    mostrarListaAmigos()
+
   }
 }
+
+function mostrarListaAmigos(){
+  let elementoHTML = document.getElementById('listaAmigos')
+  elementoHTML.innerHTML = ""
+  for(let i = 0; i <= friendsList.length - 1; i++){
+    let li = document.createElement("li")
+    li.innerHTML = friendsList[i]
+    elementoHTML.appendChild(li)
+  }
+}
+
+
+
